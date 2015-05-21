67511d097796a01983b5f1a29b657197c8f6434f
CJ Johnson <Fermentedbeats@gmail.com>
2015-05-20 19:38:58 -0700

    alorithm nightmares

--- Connect4withBeards.js
+++ Connect4withBeards.js
@@ -4,6 +4,7 @@
 // for loop drops in pre-set sized pieces within container div (inline block)
 
 var boardArray = new Array(42);
+//console.log(boardArray);
 
 function createBoard() {
 	var boardContainer = document.createElement('div');
@@ -35,50 +36,50 @@ createBoard();
 
 // 2. set 2 players
 
-var player1 = {name: 'Player 1', turn: true, color: 'red'};
-var player2 = {name: 'Player 2', turn: false, color: 'black'};
+var player1 = {name: 'Player 1', turn: true, color: 'red', number: 1};
+var player2 = {name: 'Player 2', turn: false, color: 'black', number: 2};
 
 
 // 3. alternate turns
 // turn function assigns a player and after action assigns new player
 // action lands piece in specified column (modulus of 7) in last open cell
 
-	var whosTurnIsIt = function() {
-		if (player1.turn == true) {
-			return player1;
-		}
-		else {
-			return player2;
-		}
-	}	
-
-	var switchPlayer = function() {
-		if (player1.turn == true) {
-			player1.turn = false;
-			player2.turn = true;
-		}
-		else {
-			player1.turn = true;
-			player2.turn = false;
-		}
+var whosTurnIsIt = function() {
+	if (player1.turn == true) {
+		return player1;
 	}
+	else {
+		return player2;
+	}
+}	
+
+var switchPlayer = function() {
+	if (player1.turn == true) {
+		player1.turn = false;
+		player2.turn = true;
+	}
+	else {
+		player1.turn = true;
+		player2.turn = false;
+	}
+}
 
 // 4. click on col & piece appears
 // 0..6 have event listeners: 7 diff click-here areas
 
-	var makeAChecker = function(color) {
-		var checker = document.createElement('div');
-		checker.className = color;
-		checker.className += " checker";
-		checker.style.backgroundColor = color;
-		return checker;
-	}
+var makeAChecker = function(color) {
+	var checker = document.createElement('div');
+	checker.className = color;
+	checker.className += " checker";
+	checker.style.backgroundColor = color;
+	return checker;
+}
 
-	var animateChecker = function() {
-		var currentPlayer = whosTurnIsIt();
-		var column = this.id % 7;
-		console.log(column); 
-		var newChecker = makeAChecker(currentPlayer.color);
+var animateChecker = function() {
+	var currentPlayer = whosTurnIsIt();
+	var column = this.id % 7;
+	//console.log(column); 
+	var newChecker = makeAChecker(currentPlayer.color);
 
 		// checker starts in outer space
 		// by column, look at last cell and choose the one that is not empty
@@ -92,6 +93,8 @@ var player2 = {name: 'Player 2', turn: false, color: 'black'};
 			if (vacantCheck.className == 'boardSquare vacant') {
 				vacantCheck.appendChild(newChecker);
 				vacantCheck.className = 'boardSquare occupied';
+				boardArray[squareId] = currentPlayer.number;
+				checkForWinner(squareId);
 				switchPlayer();
 				break;
 			}
@@ -103,7 +106,7 @@ var player2 = {name: 'Player 2', turn: false, color: 'black'};
 	}
 
 
-// 5 after each turns
+// 5. after each turns
 
 // GARBAGE TIME CHECK: 168 cells touched per turn
 // 	5a check row
@@ -116,8 +119,11 @@ var player2 = {name: 'Player 2', turn: false, color: 'black'};
 // MIND-BLOWING ALGORITHM: < 42 cells checked per turn.
 // check adjacent cells & brute force against the edges
 
-// destination cell index = x
-// check
+
+function checkForWinner(index) {
+
+	// destination cell index = x
+	// check
      // A. +1 & -1
      // 	while x != edge cases
 	 // B. +7 & -7
@@ -126,6 +132,50 @@ var player2 = {name: 'Player 2', turn: false, color: 'black'};
      // 	while x != edge cases
 	 // D. +8 & -8
      // 	while x != edge cases
+ 
+ 	var countingToFour = 1
+    var testCases = [1, 6, 7, 8];
+
+    // loop to check all 8 surrounding cell squares for a matching value (-/+ 1, 6, 7 or 8 indices away)
+	for (var counter0 = 0; counter0 < testCases.length; counter0++) {
+
+		    console.log("num in cell: ");
+     		console.log(boardArray[index]);
+
+		// if the player # (just-placed checker) in the array matches the value in the contiguous cell 
+		// run the next loop
+     	if (boardArray[index] === boardArray[index - testCases[counter0]] || boardArray[index] === boardArray[index+testCases[counter0]]) {
+
+    		
+     		// when a match is found, check the next cell in that direction (-/+)
+     		for(var i = index; boardArray[i] === boardArray[i-testCases[counter0]]; i-=testCases[counter0]) {
+     			countingToFour++;
+     			//console.log("loop2");
+     			if (countingToFour === 4) {
+     				console.log("winner1")
+     				return "Winner"; 			
+     			}
+
+     		}
+     		// when a match is found, check the next cell in the other direction (-/+)
+     		for (var j = index; boardArray[j] === boardArray[j+testCases[counter0]]; i+=testCases[counter0]) {
+     			//console.log("loop3");
+     			console.log(countingToFour)
+     			countingToFour++;
+     			if (countingToFour === 4) {
+     				console.log("winner2")
+     				return "Winner"; 	
+     			}
+
+     		}
+		//console.log(countingToFour);
+		countingToFour = 1;
+		}
+	}
+}
+
+
+
 
 // 6 stop game when there's a winner
 
